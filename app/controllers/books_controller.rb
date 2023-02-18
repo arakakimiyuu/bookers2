4456c6869f52e436e2ec13f99fbd5f996ce55b31
EC2 Default User <ec2-user@ip-172-31-18-41.ap-northeast-1.compute.internal>
2023-02-18 08:17:04 +0000

    [Remove]エラー修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,34 +1,48 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+     redirect_to books_path(@book)
+     flash[:notice]="You have created book successfully."
+    else
+      @books = Book.all
+      render:index
+    end
   end
 
   def index
-     @book = Book.new(book_params)
+     @book = Book.new
      @books = Book.all
      @user = current_user
   end
 
   def show
     @book = Book.find(params[:id])
+
   end
-  
+
 
   def edit
-     @book = Book.find(params[:id])
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+      flash[:notice]="You have updated book successfully."
+    else
+      render:edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
+
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,38 @@
 class UsersController < ApplicationController
 
+ def new
+   @user = User.new
+ end
+
+ def create
+   @user = User.new(user_params)
+   @user.save
+   redirect_to users_path
+   flash[:notice]="Welcome! You have signed up successfully."
+ end
+
   def index
     @users = User.all
+    @user = current_user
   end
 
   def show
     @user = User.find(params[:id])
-    @books = @user.book
-    @post_images = @user.post_images
+    @books = @user.books
   end
 
   def edit
     @user = User.find(params[:id])
-
   end
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to users_path
+    if @user.update(user_params)
+      redirect_to users_path
+      flash[:notice]="You have updated user successfully."
+    else
+      render:edit
+    end
   end
 
   private
