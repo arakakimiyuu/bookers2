3c5b39a336e1cd875127ecf5f67f977f2feb0691
EC2 Default User <kaeru.1210my@gmail.com>
2023-05-12 08:30:30 +0000

    　[Finish]コメント機能

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :books, only:[:create, :index, :show, :edit, :update,:destroy] do
     resource :favorites, only:[:create, :destroy]
-    resources :book_comments, only:[:create]
+    resources :book_comments, only:[:create, :destroy]
   end
   resources :users, only:[:new, :index, :create, :show, :edit,:update]
 
