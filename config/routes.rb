a563310678f37beba0b5d37d81c7316698f30b90
EC2 Default User <kaeru.1210my@gmail.com>
2023-05-13 09:55:25 +0000

    　[Add]follow機能

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,10 @@ Rails.application.routes.draw do
     resource :favorites, only:[:create, :destroy]
     resources :book_comments, only:[:create, :destroy]
   end
-  resources :users, only:[:new, :index, :create, :show, :edit,:update]
+  resources :users, only:[:new, :index, :create, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
 
 end
