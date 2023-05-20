780ec160af5d3541c105c4f7b0486070ee418767
EC2 Default User <kaeru.1210my@gmail.com>
2023-05-20 11:42:35 +0000

    　[Add]いいねの合計カウントが多い順に投稿を表示

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,8 @@ class Book < ApplicationRecord
 
  belongs_to :user
  has_many :favorites, dependent: :destroy
+ #favoritesモデルを介してuserモデルのデータを持ってくる記述
+ has_many :favorited_users, through: :favorites, source: :user
  has_many :book_comments, dependent: :destroy
 
   validates :title, presence: true
