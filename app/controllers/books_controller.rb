1191dabf2053f48289f0b0590279e862ff07c3e6
EC2 Default User <kaeru.1210my@gmail.com>
2023-05-21 12:07:23 +0000

    　[Add]いいねの合計カウントが多い順に投稿を表示のコメントアウト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,11 +20,17 @@ class BooksController < ApplicationController
      @book = Book.new
      @books = Book.all
      @user = current_user
+     #Time.current はconfig/application.rbに設定してあるタイムゾーンを元に現在日時を取得
+     #at_end_of_day は1日の終わりを23:59に設定
      to = Time.current.at_end_of_day
+     #at_beginning_of_day　は1日の始まりの時刻を0:00に設定
      from = (to - 6.day).at_beginning_of_day
+     #ユーザーの情報がfavorited_usersに格納されて、includesの引数に入れ事前にユーザーのデータを取ってくる
      @books = Book.includes(:favorited_users).
+     #ブロック変数 |x| を定義しないと、2よりも10,11の方が小さいと判定されるためこのように対処する
       sort_by {|x|
         x.favorited_users.includes(:favorites).where(created_at: from...to).size
+      #reverseをつけることで昇順、降順を入れ替えることができる
       }.reverse
      @book = Book.new
   end
