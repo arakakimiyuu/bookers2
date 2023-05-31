6a4097d71781757063ce451802810cf8cac33a39
EC2 Default User <kaeru.1210my@gmail.com>
2023-05-31 08:40:57 +0000

    　[Add]今週と先週の投稿数の差を表示させる

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,6 +53,8 @@ class UsersController < ApplicationController
     @newbook = Book.new
     @today_book = @books.created_today
     @yesterday_book = @books.created_yesterday
+    @this_week_book = @books.created_this_week
+    @last_week_book = @books.created_last_week
   end
 
   def edit
