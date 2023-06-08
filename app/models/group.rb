e085640b86f68b9eabf4004628be5006a34af432
EC2 Default User <kaeru.1210my@gmail.com>
2023-06-08 07:38:35 +0000

    　[Add]グループ新規、一覧画面を作成

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
-  
+
   has_many :group_users, dependent: :destroy
   belongs_to :owner, class_name: 'User'
   has_many :users, through: :group_users
@@ -7,9 +7,9 @@ class Group < ApplicationRecord
   validates :name, presence: :true
   validates :introduction, presence: :true
   has_one_attached :group_image
-  
+
   def is_owned_by?(user)
     owner.id == user.id
   end
-  
+
 end
