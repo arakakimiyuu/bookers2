f4dfc90bfb4015de82954edba6f895798dd139d6
EC2 Default User <kaeru.1210my@gmail.com>
2023-06-16 06:58:01 +0000

    　[Add]starカラムの追加,indexページ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_07_045655) do
+ActiveRecord::Schema.define(version: 2023_06_16_041936) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2023_06_07_045655) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.string "star"
   end
 
   create_table "entries", force: :cascade do |t|
